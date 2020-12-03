21f16412a7ed7961b78dd02313844bf4c49543b0
didi <didi@leaf.space>
2020-12-03 12:20:25 +0100

    day 3 part 2 🏂

--- aoc/day_3/day_3.py
+++ aoc/day_3/day_3.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import List, Union
+from typing import List, Tuple, Union
+from math import prod
 
 with open("aoc/day_3/input.txt", "r") as f:
     _input = f.read()
@@ -49,6 +50,7 @@ class Slope:
     slope_lines: List[SlopeLine]
     _coord_right = 0
     _coord_down = 0
+    _rules = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
     @classmethod
     def from_raw(cls, raw: str) -> "Slope":
@@ -59,8 +61,6 @@ class Slope:
     def _get(
         self, coord_right: int, coord_down: int
     ) -> Union[TreeSquare, OpenSquare]:
-        # should raise IndexError if slope is too short to the right.
-        # return self.slope_lines[coord_down].parsed[coord_right]
         try:
             _down = self.slope_lines[coord_down]
         except IndexError as e:
@@ -74,17 +74,25 @@ class Slope:
         for line in self.slope_lines:
             line.duplicate()
 
-    def _increment_coords(self) -> None:
+    def _increment_coords_part_1(self) -> None:
         self._coord_right += 3
         self._coord_down += 1
 
-    def count_trees(self) -> int:
+    def _increment_coords_by_rule(self, rule: Tuple[int, int]) -> None:
+        self._coord_right += rule[0]
+        self._coord_down += rule[1]
+
+    def _reset_coords(self) -> None:
+        self._coord_right = 0
+        self._coord_down = 0
+
+    def count_trees_part_1(self) -> int:
         # starting point shall be OpenSquare
         assert isinstance(
             self._get(self._coord_right, self._coord_down), OpenSquare
         )
         tree_count = 0
-        self._increment_coords()
+        self._increment_coords_part_1()
         while True:
             try:
                 tree_or_open = self._get(self._coord_right, self._coord_down)
@@ -97,15 +105,56 @@ class Slope:
                 print("Slope should be over")
                 return tree_count
             else:
-                self._increment_coords()
+                self._increment_coords_part_1()
+
+    def count_trees_part_2(self) -> Tuple[List[int], int]:
+        tree_count_list = []
+        for rule in self._rules:
+            tree_count = 0
+            self._reset_coords()
+            assert isinstance(
+                self._get(self._coord_right, self._coord_down), OpenSquare
+            )
+            self._increment_coords_by_rule(rule)
+            while True:
+                try:
+                    tree_or_open = self._get(
+                        self._coord_right, self._coord_down
+                    )
+                    if isinstance(tree_or_open, TreeSquare):
+                        tree_count += 1
+                except SlopeShallBeExtended:
+                    print("Extending the slope to the right")
+                    self._repeat_to_the_right()
+                except SlopeIsOver:
+                    print("Slope should be over")
+                    tree_count_list.append(tree_count)
+                    break
+                else:
+                    self._increment_coords_by_rule(rule)
+        return tree_count_list, prod(tree_count_list)
 
 
 if __name__ == "__main__":
+    # part 1
     # test
     slope = Slope.from_raw(_test)
-    trees = slope.count_trees()
+    trees = slope.count_trees_part_1()
     print(f"TEST - Encountered {trees} trees")
     # prod
     slope = Slope.from_raw(_input)
-    trees = slope.count_trees()
+    trees = slope.count_trees_part_1()
     print(f"PROD - Encountered {trees} trees")
+    # part 2
+    # test
+    slope = Slope.from_raw(_test)
+    tree_list, trees_multiplied = slope.count_trees_part_2()
+    print(
+        f"TEST - Encountered {tree_list} trees, multiplied they give {trees_multiplied}"
+    )
+    # prod
+    slope = Slope.from_raw(_input)
+    tree_list, trees_multiplied = slope.count_trees_part_2()
+    print(
+        f"PROD- Encountered {tree_list} trees, multiplied they give {trees_multiplied}"
+    )
